9fa878f6bbfdcad12be9a6cf62b4a367ebb2df3e
JDavidHR <simond202020@gmail.com>
2024-07-02 13:20:29 -0500

    ADD - app colors

---
+++ lib/core/colors.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const Color primary = Color(0xFF8BC34A);
+  static const Color onPrimary = Color(0xFFFFFFFF);
+  static const Color primaryContainer = Color(0xFF3700B3);
+  static const Color onPrimaryContainer = Color(0xFFFFFFFF);
+
+  static const Color secondary = Color(0xFF03DAC6);
+  static const Color onSecondary = Color(0xFF000000);
+  static const Color secondaryContainer = Color(0xFF018786);
+  static const Color onSecondaryContainer = Color(0xFF000000);
+
+  static const Color tertiary = Color(0xFFBB86FC);
+  static const Color onTertiary = Color(0xFF000000);
+  static const Color tertiaryContainer = Color(0xFF3700B3);
+  static const Color onTertiaryContainer = Color(0xFFFFFFFF);
+
+  static const Color error = Color(0xFFB00020);
+  static const Color onError = Color(0xFFFFFFFF);
+  static const Color errorContainer = Color(0xFF3700B3);
+  static const Color onErrorContainer = Color(0xFFFFFFFF);
+
+  static const Color background = Color(0xFF121212);
+  static const Color onBackground = Color(0xFFFFFFFF);
+
+  static const Color surface = Color(0xFF121212);
+  static const Color onSurface = Color(0xFFFFFFFF);
+
+  static const Color surfaceVariant = Color(0xFF121212);
+  static const Color onSurfaceVariant = Color(0xFF000000);
+
+  static const Color outline = Color(0xFFBB86FC);
+  static const Color shadow = Color(0xFF000000);
+  static const Color inverseSurface = Color(0xFF121212);
+  static const Color onInverseSurface = Color(0xFFFFFFFF);
+  static const Color inversePrimary = Color(0xFF3700B3);
+  static const Color surfaceTint = Color(0xFF6200EE);
+
+  static const Color neutral = Color(0xFF1E1E1E);
+  static const Color onNeutral = Color(0xFFEEEEEE);
+}
